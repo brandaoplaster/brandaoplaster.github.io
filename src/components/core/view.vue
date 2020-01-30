f00b13e4ccfe84b233c6d71fedcf66bcfc7bc386
brandaoplaster <brandaoplaster@gmail.com>
2020-01-30 17:21:12 -0300

    Adding skill component

--- src/components/core/view.vue
+++ src/components/core/view.vue
@@ -5,6 +5,7 @@
             <section-home></section-home>
             <section-about></section-about>
             <section-experience></section-experience>
+            <section-skill></section-skill>
         </div>
      </div>
 </template>
@@ -18,7 +19,7 @@
             CoreToolbar: () => import('@/components/core/toolbar'),
             SectionHome: () => import('@/components/section/home'),
             SectionAbout: () => import('@/components/section/about'),
-            SectionExperience: () => import('@/components/section/experience'),
+            SectionSkill: () => import('@/components/section/skill'),
         }  
     }
 </script>
