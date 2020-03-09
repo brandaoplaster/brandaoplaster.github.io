d8613380e873c56b7f8e586ae515a916ea5df2c9
brandaoplaster <brandaoplaster@gmail.com>
2020-03-09 16:47:09 -0300

    Importing fonts from google fonts and layout for the about component

--- src/components/section/about.vue
+++ src/components/section/about.vue
@@ -2,18 +2,18 @@
   <section class="hero home" id="about">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">About</h1>
-        <h2 class="subtitle">Hero subtitle</h2>
+        <h1 class="title has-text-grey-dark has-text-left is-family-monospace">
+          about
+        </h1>
 
         <article class="message">
-          <div class="message-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-            porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam 
-            gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac
-            <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
-            porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
-            vehicula et sem eget, facilisis sodales sem.
+          <div class=" has-text-justified">
+            Graduado em sistema de informação pelo instituto Federal do Espirito Santo,
+            focado em desenvolvimento web, em especial <strong>Ruby on Rails</strong>, além de interessa-se
+            por <strong>Laravel</strong>, <strong>Angular</strong>, <strong>Vue</strong> e <strong>Reactjs</strong>.
+            <br />
+            <br />
+            Sou desenvolvedor freelance em certos momentos, dependendo do meu tempo e disponibilidade.
           </div>
         </article>
 
@@ -28,7 +28,12 @@ export default {};
 
 <style scoped>
     #about {
-        min-height: 100vh;
-        background-color: blueviolet;
+      min-height: 100vh;
+    }
+    .title {
+      border-bottom: 2px solid #cccccc;
+    }
+    .message {
+      font-family: 'Oxanium', cursive;
     }
 </style>
